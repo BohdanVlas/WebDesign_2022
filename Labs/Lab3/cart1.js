e8fc025d713f75f3b61fdccddb3611ab8e18bf23
BohdanVlas <bodik.vlasenko@gmail.com>
2022-10-06 11:53:54 +0300

    Lab3 added

---
+++ Labs/Lab3/cart1.js
@@ -0,0 +1,11 @@
+function calcCartWeight() {
+	const cartWrapper = document.querySelector('.cart-wrapper');
+	const weightElements = cartWrapper.querySelectorAll('.weight__currency');
+	const totalWeightEl = document.querySelector('.total-weight');
+	let weightTotal = 0;
+	weightElements.forEach(function (item) {
+		const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
+		weightTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
+	});
+	totalWeightEl.innerText = weightTotal;
+}
