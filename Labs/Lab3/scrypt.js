d9ce7798b02ae5fe8ed3213c64437376a70fb7a4
BohdanVlas <bodik.vlasenko@gmail.com>
2022-10-06 11:51:01 +0300

    Lab3 updated

--- Labs/Lab3/scrypt.js
+++ Labs/Lab3/scrypt.js
@@ -14,62 +14,7 @@ window.addEventListener('click', function (event) {
         else if (event.target.closest('.cart-wrapper') && parseInt(cnt.innerText) === 1) {
             event.target.closest('.cart-item').remove();
             toggleCartStatus();
-            calcCartPriceAndDelivery();
-        }
-    }
-    if (event.target.hasAttribute('data-cart')) {
-        const card = event.target.closest('.card')
-        const rocketinfo = {
-            id: card.dataset.id,
-            imSrc: card.querySelector('.rocket-img').getAttribute('src'),
-            title: card.querySelector('.rocket-title').innerText,
-            rockets: card.querySelector('[data-rockets-in-box]').innerText,
-            weight: card.querySelector('.weight').innerText,
-            counter: card.querySelector('[data-counter]').innerText,
-        }
-        const itemInCart = cartWrapper.querySelector(`[data-id="${rocketInfo.id}"]`);
-        if (rocketInCart) {
-            const counterElement = rocketInCart.querySelector('[data-counter]');
-            counterElement.innerText = parseInt(counterElement.innerText) + parseInt(rocketInfo.counter);
-        }
-        else {
-            const cartRocketHTML = `<div class="cart-item" data-id="${rocketinfo.id}">
-                                    <div class="cart-item__top">
-                                        <div class="cart-item__img">
-                                            <img src="${rocketinfo.imgSrc}" alt="${rocketinfo.title}">
-                                        </div>
-                                    <div class="cart-item__desc">
-                                        <div class="cart-item__title">${rocketinfo.title}</div>
-                                        <div class="cart-item__weight">${rocketinfo.itemsInBox} / ${rocketinfo.weight}</div>
-                                        <div class="cart-item__details">
-                                            <div class="items items--small counter-wrapper">
-                                                <div class="items__control" data-action="minus">-</div>
-                                                <div class="items__current" data-counter="">${rocketinfo.counter}</div>
-                                                <div class="items__control" data-action="plus">+</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`;
-            cartWrapper.insertAdjacentHTML('beforeend', cartRocketHTML);
+            calcCartWeight();
         }
     }
 })
-
-const cartWrapper = document.querySelector('.cart-wrapper');
-const cartEmptyBadge = document.querySelector('[data-cart-empty]');
-const orderForm = document.querySelector('#order-form');
-if (cartWrapper.children.length > 0) {
-    cartEmptyBadge.classList.add('none');
-    orderForm.classList.remove('none');
-} else {
-    cartEmptyBadge.classList.remove('none');
-    orderForm.classList.add('none');
-}
-const totalWeightEl = document.querySelector('.total-weight');
-let weightTotal = 0;
-weightElements.forEach(function (rocket) {
-    const amountEl = item.closest('.cart-rocket').querySelector('[data-counter]');
-    weightTotal += parseInt(rocket.innerText) * parseInt(amountEl.innerText);
-});
-totalWeightEl.innerText = weightTotal;
