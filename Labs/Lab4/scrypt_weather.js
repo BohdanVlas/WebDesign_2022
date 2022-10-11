718d85ee4ff12675879a7fa418a1e69fe9db0bf6
BohdanVlas <bodik.vlasenko@gmail.com>
2022-10-11 21:41:35 +0300

    Lab4 updated

--- Labs/Lab4/scrypt_weather.js
+++ Labs/Lab4/scrypt_weather.js
@@ -1,31 +1,17 @@
 const weatherBlock = document.querySelector('#weather');
+const Server = 'https://knu_god:1ta2Wc1HEk@api.meteomatics.com/2022-10-11T13:00:00.000+03:00/t_2m:C/50.4500336,30.5241361/html?model=mix'
 
-async function loadWeather(e) {
-    weatherBlock.innerHTML = `<div class="weather_loading">
-                                <img src="./Images/loading....gif" alt="loading...">
-                              </div>`;
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("http://knu_god:1ta2Wc1HEk@api.meteomatics.com//t_2m:C/50.4500336,30.5241361/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
-    const Server = 'https://knu_god:1ta2Wc1HEk@api.meteomatics.com/2022-10-11T13:00:00.000+03:00/t_2m:C/50.4500336,30.5241361/html?model=mix'
-
-    const response = await fetch(Server, {
-        method: 'GET',
-    })
-
-    const responseResult = await response.json();
-
-    if (response.ok) {
-        getWeather(responseResult);
-    }
-    else {
-        weatherBlock.innerHTML = responseResult.message;
-    }
-
-}
-
-function getWeather(data) {
-    console.log(data)
-}
-
-if (weatherBlock) {
-    loadWeather();
-}
+    fetch("http://knu_god:1ta2Wc1HEk@api.meteomatics.com/2022-10-11T00:00:00ZP3D:PT6H/msl_pressure:hPa/50.4500336,30.5241361/json", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
