5af922d83d374066ad1122b73ae3c60766c33a2a
BohdanVlas <bodik.vlasenko@gmail.com>
2022-10-12 20:34:20 +0300

    Lab4 completed

--- Labs/Lab4/scrypt_weather.js
+++ Labs/Lab4/scrypt_weather.js
@@ -1,17 +1,49 @@
 const weatherBlock = document.querySelector('#weather');
-const Server = 'https://knu_god:1ta2Wc1HEk@api.meteomatics.com/2022-10-11T13:00:00.000+03:00/t_2m:C/50.4500336,30.5241361/html?model=mix'
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("http://knu_god:1ta2Wc1HEk@api.meteomatics.com//t_2m:C/50.4500336,30.5241361/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+async function loadWeather(e) {
+    weatherBlock.innerHTML = '<div class="weather_loading"> <img src="./Images/loading....gif" alt="loading"> </div>';
 
-    fetch("http://knu_god:1ta2Wc1HEk@api.meteomatics.com/2022-10-11T00:00:00ZP3D:PT6H/msl_pressure:hPa/50.4500336,30.5241361/json", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+    const server = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=Kyiv&appid=a3b09cbc0730d4c30dd78880ca1799da'
+
+    const response = await fetch(server, {
+        method: 'GET',
+    });
+
+    const responseResult = await response.json();
+
+    if(response.ok) {
+        getWeather(responseResult);
+    }
+    else {
+        weatherBlock.innerHTML = responseResult.message;
+    }
+}
+
+function getWeather(data) {
+
+    const location = data.name;
+    const temp = Math.round(data.main.temp);
+    const feelsLike = Math.round(data.main.feels_like);
+    const weatherStatus = data.weather[0].main;
+    const weatherIcon = data.weather[0].icon;
+
+    const template = `<div class="weather" id="weather">
+                         <div class="weather_header">
+                             <div class="weather_main">
+                                 <div class="wether_city">${location}</div>
+                                <div class="weather_status">${weatherStatus}</div>
+                             </div>
+                             <div class="weather_icon">
+                                 <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}">
+                             </div>
+                         </div>
+                         <div class="weather_temp">${temp}</div>
+                         <div class="weather_feels-like">Fells like: ${feelsLike}</div>
+                     </div>`;
+
+    weatherBlock.innerHTML = template;
+}
+
+if (weatherBlock) {
+    loadWeather();
+}
