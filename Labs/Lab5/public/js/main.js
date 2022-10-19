1d3d1545a1781f02ff364ecf5bb1727f1912294c
BohdanVlas <bodik.vlasenko@gmail.com>
2022-10-20 00:17:09 +0300

    Lab5 updated

--- Labs/Lab5/public/js/main.js
+++ Labs/Lab5/public/js/main.js
@@ -1,18 +1,35 @@
-const productsContainer = document.querySelector('#box-container');
+async function addproduct(id, title, power, imgSrc) {
+    let product = document.createElement('div')
+    let container = document.querySelector('.#box-container')
 
-function buttonClick(){
+    let ViewerContent = `<div class="box" data-id="${id}">
+                            <img class="" src="${imgSrc}">
+                            <h3 class="title">${title}</h3>
+                            <p><small>Кількість в наборі: ${itemsInBox}</small><small data-items-in-box class="text-muted"></small><small>шт</small></p>
+                            <p><small>Потужність: </small><small class="power">${power}</small><small>ВТ</small></p>
+                            <p><small>Артикул: </small><small class="id">${id}</small></p>
+                        </div>`
+    product.innerHTML = ViewerContent;
+    container.append(product);
+}
+
+async function Get_Product() 
+{
     fetch("http://localhost:3000/products").then(
         (response)=>{
-            return response.text();
+            return response.text()
         }
-    ).then((function (item) {
-        const productHTML = `<div class="box" data-id="${id}">
-                                <img class="" src="${imgSrc}">
-                                <h3 class="title">${title}</h3>
-                                <p><small>Кількість в наборі: ${itemsInBox}</small><small data-items-in-box class="text-muted"></small><small>шт</small></p>
-                                <p><small>Потужність: </small><small class="power">${power}</small><small>ВТ</small></p>
-                                <p><small>Артикул: </small><small class="id">${id}</small></p>
-                                </div>`;
-        productsContainer.insertAdjacentHTML('beforeend', productHTML);
-    }))
+    ).then((text)=>{
+        let aboutArray = JSON.parse(text)
+        aboutArray.forEach(element => {
+
+            id = `${element.id}`
+            tit = `${element.title}`
+            pow = `${element.power}`
+            img = `${element.imgSrc}`
+
+            addproduct(id, tit, pow, imgSrc)
+        })
+    })
 }
+Get_Product();
